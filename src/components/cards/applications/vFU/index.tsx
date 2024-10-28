c05bf69cceca91468c5fd9701bfc3436d55911a8
Artyom Kachyro <artyomkachyro2006@gmail.com>
2024-10-28 06:05:46 +0300

    commit for AA<3

--- src/components/cards/applications/vFU/index.tsx
+++ src/components/cards/applications/vFU/index.tsx
@@ -3,62 +3,44 @@ import classes from "./classes.module.scss"
 import { EditOutlined, DeleteOutlined, QuestionCircleOutlined } from "@ant-design/icons";
 import { Popconfirm } from "antd";
 
+
 const FullCard: React.FC<{
-    profession: string,
-    description: string,
-    location: string,
-    salary: string,
-    appId: string,
-    withEdit: boolean,
-    setEditingId: Dispatch<SetStateAction<string>>,
-    handleDeleting: () => void,
+    profession: string;
+    description: string;
+    location: string;
+    salary: string;
+    appId: string;
+    withEdit: boolean;
+    setEditingId?: Dispatch<SetStateAction<string>>;
+    handleDeleting?: () => void;
 }> = ({ profession, description, location, salary, appId, withEdit, setEditingId, handleDeleting }) => {
-
     return (
-        <>
-            <div className={classes.cart_container}>
-                        <>
-                            <div className={classes.name}>
-                                <p>{profession}</p>
-                                {withEdit &&
-                                    <div>
-                                        <EditOutlined onClick={() => setEditingId(appId)}/>
-                                        <Popconfirm
-                                            title="DELETING"
-                                            description="Are you sure to delete this application?"
-                                            icon={<QuestionCircleOutlined style={{color: 'red'}}/>}
-                                            cancelText="yes"
-                                            okText="no"
-                                            onCancel={handleDeleting}
-                                        >
-                                            <DeleteOutlined/>
-                                        </Popconfirm>
-                                    </div>
-                                }
-
-                            </div>
-
-                            <div className={classes.location}>
-                                <p className={classes.heading}>Location: </p>
-                                <p className={classes.subheading}>{location}</p>
-                            </div>
-
-                            <div className={classes.salary}>
-                                <p className={classes.heading}>Starting salary: </p>
-                                <p className={classes.subheading}>{salary}</p>
-                            </div>
-
-                            <div className={classes.description}>
-                                <p>{description}</p>
-                            </div>
-
-                            <div className={classes.id}>
-                                <p>{appId}</p>
-                            </div>
-                        </>
+        <div className={classes.cart_container}>
+            <div className={classes.name}>
+                <p>{profession}</p>
+                {withEdit && setEditingId && handleDeleting && (
+                    <div>
+                        <EditOutlined onClick={() => setEditingId(appId)} />
+                        <Popconfirm
+                            title="DELETING"
+                            description="Are you sure to delete this application?"
+                            icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+                            cancelText="yes"
+                            okText="no"
+                            onCancel={handleDeleting}
+                        >
+                            <DeleteOutlined />
+                        </Popconfirm>
+                    </div>
+                )}
             </div>
-        </>
-    )
-}
+
+            <p className={classes.location}>Location: {location}</p>
+            <p className={classes.salary}>Starting salary: {salary}</p>
+            <p className={classes.description}>{description}</p>
+            <p className={classes.id}>{appId}</p>
+        </div>
+    );
+};
 
 export default FullCard;
--- src/components/cards/cvs/vFU/index.tsx
+++ src/components/cards/cvs/vFU/index.tsx
@@ -12,38 +12,36 @@ const FullCard: React.FC<{
     salary: string;
     cvsId: string,
     withEdit: boolean,
-    setEditingId: Dispatch<SetStateAction<string>>;
-    handleDeleting: () => void;
+    setEditingId?: Dispatch<SetStateAction<string>>;
+    handleDeleting?: () => void;
 }> = ({ name, profession, description, location, salary, cvsId, setEditingId, withEdit, handleDeleting }) => {
 
     return (
         <>
             <div className={classes.cart_container}>
-                        <>
-                            <div className={classes.profession}>
-                                <p>{profession}</p>
-                                {withEdit &&
-                                    <div>
-                                        <EditOutlined onClick={() => setEditingId(cvsId)}/>
-                                        <Popconfirm
-                                            title="DELETING"
-                                            description="Are you sure to delete this application?"
-                                            icon={<QuestionCircleOutlined style={{color: 'red'}}/>}
-                                            cancelText="yes"
-                                            okText="no"
-                                            onCancel={handleDeleting}
-                                        >
-                                            <DeleteOutlined/>
-                                        </Popconfirm>
-                                    </div>
-                                }
-                            </div>
-                            <div className={classes.name}>Full name: {name}</div>
-                            <div className={classes.location}>Location: {location}</div>
-                            <div className={classes.salary}>Wanted salary: {salary}</div>
-                            <div className={classes.description}>{description}</div>
-                            <div className={classes.id}>{cvsId}</div>
-                        </>
+                <div className={classes.profession}>
+                    <p>{profession}</p>
+                    {withEdit && setEditingId && handleDeleting &&
+                        <div>
+                            <EditOutlined onClick={() => setEditingId(cvsId)}/>
+                            <Popconfirm
+                                title="DELETING"
+                                description="Are you sure to delete this application?"
+                                icon={<QuestionCircleOutlined style={{color: 'red'}}/>}
+                                cancelText="yes"
+                                okText="no"
+                                onCancel={handleDeleting}
+                            >
+                                <DeleteOutlined/>
+                            </Popconfirm>
+                        </div>
+                    }
+                </div>
+                <p className={classes.name}>Full name: {name}</p>
+                <p className={classes.location}>Location: {location}</p>
+                <p className={classes.salary}>Wanted salary: {salary}</p>
+                <p className={classes.description}>{description}</p>
+                <p className={classes.id}>{cvsId}</p>
             </div>
         </>
     )
